c6b31f5067c9bd478c867bf55f8bb5cd0720c68c
ismail <ismail.samir52@gmail.com>
2020-03-17 17:58:38 +0200

    use lock_guard insead of unique_lock

--- thread_pool/include/pool/Queue.h
+++ thread_pool/include/pool/Queue.h
@@ -38,30 +38,29 @@ namespace pool
 	template<typename T>
 	T& Queue<T>::front()
 	{
+		std::lock_guard<std::mutex> locker(_mutex);
 		assert(!this->empty() && "Queue is empty");
-
-		std::unique_lock<std::mutex> locker(_mutex);
 		return this->_data[this->_front];
 	}
 
 	template<typename T>
 	size_t Queue<T>::size()
 	{
-		std::unique_lock<std::mutex> locker(_mutex);
+		std::lock_guard<std::mutex> locker(_mutex);
 		return this->_size;
 	}
 
 	template<typename T>
 	bool Queue<T>::empty()
 	{
-		std::unique_lock<std::mutex> locker(_mutex);
+		std::lock_guard<std::mutex> locker(_mutex);
 		return _size == 0;
 	}
 
 	template<typename T>
 	void Queue<T>::clear()
 	{
-		std::unique_lock<std::mutex> locker(_mutex);
+		std::lock_guard<std::mutex> locker(_mutex);
 		this->_front = -1;
 		this->_rear = -1;
 	}
@@ -69,31 +68,27 @@ namespace pool
 	template<typename T>
 	void Queue<T>::push(const T& val)
 	{
+		std::lock_guard<std::mutex> locker(_mutex);
 		assert((_rear + 1) % MAX_CAPACITY != _front && "Queue is full");
-		{
-			std::unique_lock<std::mutex> locker(_mutex);
-			if (_rear == -1)
-				_front = _rear = 0;
-			else
-				_rear = (_rear + 1) % MAX_CAPACITY;
+		if (_rear == -1)
+			_front = _rear = 0;
+		else
+			_rear = (_rear + 1) % MAX_CAPACITY;
 
-			this->_data[_rear] = val;
-			++_size;
-		}
+		this->_data[_rear] = val;
+		++_size;
 	}
 
 	template<typename T>
 	void Queue<T>::pop()
 	{
+		std::lock_guard<std::mutex> locker(_mutex);
 		assert(!this->empty() && "Queue is empty");
-		{
-			std::unique_lock<std::mutex> locker(_mutex);
-			if (_front == _rear)
-				_front = _rear = -1;
-			else
-				_front = (_front + 1) % MAX_CAPACITY;
+		if (_front == _rear)
+			_front = _rear = -1;
+		else
+			_front = (_front + 1) % MAX_CAPACITY;
 
-			--_size;
-		}
+		--_size;
 	}
 }
